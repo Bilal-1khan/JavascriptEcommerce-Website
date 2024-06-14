51a3dd8d1817aadf82af78b4228d27ea1fb08a05
bilal <bilalsuri9898@gmail.com>
2024-06-14 15:20:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const cartContainer = document.getElementById("cartContainer")
+const cartUpdateBtn = document.querySelectorAll("#cartUpdateBtn")
+
+// cartContainer.forEach((_,indexNo)=>{
+//     let spanCountElement = document.createElement("span")
+//     spanCountElement.innerHTML = indexNo
+//     spanCountElement.style.color = "red"
+//     cartContainer 
+// })
+
