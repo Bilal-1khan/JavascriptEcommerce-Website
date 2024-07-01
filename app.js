6cec5a45580015800e71879b78ceb727589a5712
bilal <bilalsuri9898@gmail.com>
2024-07-01 09:52:45 +0500

    add functionality

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
 // global variables
-const cartUpdateBtn = document.querySelectorAll(".cartBtn")
-const countUpdate = document.getElementById("countUpdate")
-let cardContainers = document.querySelectorAll(".cardContainer")
+const cartUpdateBtn = document.querySelectorAll(".cartBtn");
+const countUpdate = document.getElementById("countUpdate");
+let cardContainers = document.querySelectorAll(".cardContainer");
 
 // cart dropdown functionality
-const dropDownBtn = document.getElementById("dropDownBtn")
-const dropDownList = document.getElementById("dropDownList")
-dropDownBtn.addEventListener("click",function () {
-    dropDownList.classList.toggle("displayBlock")
-})
+const dropDownBtn = document.getElementById("dropDownBtn");
+const dropDownList = document.getElementById("dropDownList");
+dropDownBtn.addEventListener("click", function () {
+  dropDownList.classList.toggle("displayBlock");
+});
 
 // cart data functionality
-let cardData = []
-const ulBox = document.getElementById("ulBox")
-
+let cardImgData = [];
+let cardProductData = [];
+const ulBox = document.getElementById("ulBox");
 // cart data update in list
-cartUpdateBtn.forEach((curBtnEle,index)=>{
-    curBtnEle.addEventListener("click",function () {
-      let cartListItems = document.createElement("li")
-      cardContainers.forEach((cardContainerElem) => {
-        let productImage = cardContainerElem.querySelector(".image img");
-        cardData.push(productImage.getAttribute("src"));
+cartUpdateBtn.forEach((curBtnEle, index) => {
+  curBtnEle.addEventListener("click", function () {
+    let cartListItems = document.createElement("li")
+    
+    let cardContainerElem = cardContainers[index]
+    let productImage = cardContainerElem.querySelector(".image img");
+    let productName = cardContainerElem.querySelector(".productName").innerText;
+    
+    cardImgData.push(productImage.getAttribute("src"));
+    cardProductData.push(productName);
+
+    cartListItems.innerHTML = `<img src="${cardImgData[index]}" alt="Product Image" style="width:50px;height:50px;"> 
+      <span>${cardProductData[index]}</span>
+      <span><i id="deleteCartData" class="fa-solid fa-xmark"></i></span>`;
+    cartListItems.classList.add("cartlist-items");
+
+    // update counter number
+    let countNumber = [];
+    countUpdate.innerText = countNumber.length
+    
+    // cartUpdateBtn.forEach((curElem) => {
+    //   curElem.addEventListener("click", function () {
+    //     countNumber.push(cardContainers);
+    //     countUpdate.innerHTML = countNumber.length;
+    //   });
+    // })
+
+    let cartBtns =  cartUpdateBtn[index]
+    cartBtns.addEventListener("click",function () {
+      countNumber.push(cardContainers)
+      countUpdate.innerText = countNumber.length
+    })
+
+    // delete list
+    let deleteData = cartListItems.querySelector("#deleteCartData");
+    deleteData.addEventListener("click", function () {
+      cartListItems.remove();
     });
-      cartListItems.innerHTML = `<img src="${cardData[index]}" alt="Product Image" style="width:50px;height:50px;"> <span><i id="deleteCartData" class="fa-solid fa-xmark"></i></span>`;
-      cartListItems.classList.add("cartlist-items")
-      let deleteData = cartListItems.querySelector("#deleteCartData")
-      deleteData.addEventListener("click",function () {
-            cartListItems.remove()      
-            countUpdate.innerHTML = Number(cardData.index) - Number(cardData.index)       
-      })
-      ulBox.appendChild(cartListItems)
-      console.log(ulBox.appendChild(cartListItems));
-    })
-})
 
-// counter number update
-let countNumber = []
-countUpdate.innerHTML = countNumber.length
+    ulBox.appendChild(cartListItems);
+  });
+});
 
-cartUpdateBtn.forEach((curElem)=>{
-    curElem.addEventListener('click',function () {
-        countNumber.push(cardContainers)
-        countUpdate.innerHTML = countNumber.length
-    })
-})
+
+// const testingArray = [1,2,3,4,5,6,7,8,9,10]
+
+// testingArray.push(11,12)
+// console.log(testingArray);
